00f3a11519fa3db67e5bf820d1fac3056070877a
NattoMillin <zz112ggww@icloud.com>
2023-06-16 23:22:12 +0900

    Next Error Handling

---
+++ src/app/@team/page.tsx
@@ -0,0 +1,14 @@
+const Page = () => {
+  return (
+    <div className="m-4">
+      <h1>Parallel Routes の設定</h1>
+      <p>
+        Parallel Routing を利用することで 1 つの Layout で複数の Page
+        コンポーネントを表示させることができます。
+      </p>
+      <h1 className="text-2xl">Team</h1>
+    </div>
+  );
+};
+
+export default Page;
